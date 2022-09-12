1e911b8c72204e45051e7baa817f8f87b620c33f
Jackson Davis <davisj564@gmail.com>
2022-09-12 10:32:06 -0400

    Initial Commit

---
+++ Shared/RiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiftApp.swift
+//  Shared
+//
+//  Created by Jackson Davis on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
